0642ac53fef4fb31bf64930cfcbdd010f1184961
Yuniya Kim <yuniyakim@gmail.com>
2022-08-15 13:57:24 +0300

    Add simple navigation using React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import {Navigate, Route, Routes} from "react-router-dom";
+import Login from "./components/Login";
+import ContactList from "./components/ContactList";
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/contacts" element={<ContactList />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </div>
   );
 };
