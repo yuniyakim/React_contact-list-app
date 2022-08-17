f328e4a9f2699aba8351672a4ebcb6054088e5fb
Yuniya Kim <yuniyakim@gmail.com>
2022-08-17 13:09:15 +0300

    Add contact reducer

---
+++ src/store/reducers/contactReducer.ts
@@ -0,0 +1,51 @@
+import {ContactAction, ContactActionTypes, ContactState} from "../../types/contact";
+
+const initialState: ContactState = {
+  contacts: [],
+  loading: false,
+  error: null,
+}
+
+export const contactReducer = (state = initialState, action: ContactAction): ContactState => {
+  switch (action.type) {
+    case ContactActionTypes.FETCH_CONTACTS: {
+      return {...state, loading: true};
+    }
+    case ContactActionTypes.FETCH_CONTACTS_SUCCESS: {
+      return {...state, loading: false, contacts: action.payload};
+    }
+    case ContactActionTypes.FETCH_CONTACTS_ERROR: {
+      return {...state, loading: false, error: action.payload};
+    }
+    case ContactActionTypes.ADD_CONTACT: {
+      return {...state, loading: true};
+    }
+    case ContactActionTypes.ADD_CONTACT_SUCCESS: {
+      return {...state, loading: false, contacts: [...state.contacts, action.payload]};
+    }
+    case ContactActionTypes.ADD_CONTACT_ERROR: {
+      return {...state, loading: false, error: action.payload};
+    }
+    case ContactActionTypes.EDIT_CONTACT: {
+      return {...state, loading: true};
+    }
+    case ContactActionTypes.EDIT_CONTACT_SUCCESS: {
+      return {...state, loading: false, contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact)};
+    }
+    case ContactActionTypes.EDIT_CONTACT_ERROR: {
+      return {...state, loading: false, error: action.payload};
+    }
+    case ContactActionTypes.DELETE_CONTACT: {
+      return {...state, loading: true};
+    }
+    case ContactActionTypes.DELETE_CONTACT_SUCCESS: {
+      return {...state, loading: false, contacts: state.contacts.filter(contact => contact.id !== action.payload)};
+    }
+    case ContactActionTypes.DELETE_CONTACT_ERROR: {
+      return {...state, loading: false, error: action.payload};
+    }
+    default: {
+      return state;
+    }
+  }
+}
--- src/types/contact.ts
+++ src/types/contact.ts
@@ -3,3 +3,94 @@ export interface Contact {
   surname: string;
   name: string;
 }
+
+export interface ContactState {
+  contacts: Contact[];
+  loading: boolean;
+  error: null | string;
+}
+
+export enum ContactActionTypes {
+  FETCH_CONTACTS = 'FETCH_CONTACTS',
+  FETCH_CONTACTS_SUCCESS = 'FETCH_CONTACTS_SUCCESS',
+  FETCH_CONTACTS_ERROR = 'FETCH_CONTACTS_ERROR',
+  ADD_CONTACT = 'ADD_CONTACT',
+  ADD_CONTACT_SUCCESS = 'ADD_CONTACT_SUCCESS',
+  ADD_CONTACT_ERROR = 'ADD_CONTACT_ERROR',
+  EDIT_CONTACT = 'EDIT_CONTACT',
+  EDIT_CONTACT_SUCCESS = 'EDIT_CONTACT_SUCCESS',
+  EDIT_CONTACT_ERROR = 'EDIT_CONTACT_ERROR',
+  DELETE_CONTACT = 'DELETE_CONTACT',
+  DELETE_CONTACT_SUCCESS = 'DELETE_CONTACT_SUCCESS',
+  DELETE_CONTACT_ERROR = 'DELETE_CONTACT_ERROR',
+}
+
+interface FetchContactsAction {
+  type: ContactActionTypes.FETCH_CONTACTS;
+}
+
+interface FetchContactsSuccessAction {
+  type: ContactActionTypes.FETCH_CONTACTS_SUCCESS;
+  payload: Contact[];
+}
+
+interface FetchContactsErrorAction {
+  type: ContactActionTypes.FETCH_CONTACTS_ERROR;
+  payload: string;
+}
+
+interface AddContactAction {
+  type: ContactActionTypes.ADD_CONTACT;
+}
+
+interface AddContactActionSuccessAction {
+  type: ContactActionTypes.ADD_CONTACT_SUCCESS;
+  payload: Contact;
+}
+
+interface AddContactActionErrorAction {
+  type: ContactActionTypes.ADD_CONTACT_ERROR;
+  payload: string;
+}
+
+interface EditContactAction {
+  type: ContactActionTypes.EDIT_CONTACT;
+}
+
+interface EditContactActionSuccessAction {
+  type: ContactActionTypes.EDIT_CONTACT_SUCCESS;
+  payload: Contact;
+}
+
+interface EditContactActionErrorAction {
+  type: ContactActionTypes.EDIT_CONTACT_ERROR;
+  payload: string;
+}
+
+interface DeleteContactAction {
+  type: ContactActionTypes.DELETE_CONTACT;
+}
+
+interface DeleteContactActionSuccessAction {
+  type: ContactActionTypes.DELETE_CONTACT_SUCCESS;
+  payload: number;
+}
+
+interface DeleteContactActionErrorAction {
+  type: ContactActionTypes.DELETE_CONTACT_ERROR;
+  payload: string;
+}
+
+export type ContactAction =
+  FetchContactsAction
+  | FetchContactsSuccessAction
+  | FetchContactsErrorAction
+  | AddContactAction
+  | AddContactActionSuccessAction
+  | AddContactActionErrorAction
+  | EditContactAction
+  | EditContactActionSuccessAction
+  | EditContactActionErrorAction
+  | DeleteContactAction
+  | DeleteContactActionSuccessAction
+  | DeleteContactActionErrorAction;
