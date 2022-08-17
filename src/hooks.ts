1424dc63a8bfa516c43840c17b65bb38a8e92534
Yuniya Kim <yuniyakim@gmail.com>
2022-08-17 19:35:33 +0300

    Add redux hooks

---
+++ src/hooks.ts
@@ -0,0 +1,13 @@
+import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
+import type {RootState, AppDispatch} from './store';
+import {bindActionCreators} from "@reduxjs/toolkit";
+import ActionCreators from './store/actionCreators/index';
+
+const useAppDispatch: () => AppDispatch = useDispatch;
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useAppActions = () => {
+  const dispatch = useAppDispatch();
+  return bindActionCreators(ActionCreators, dispatch);
+}
