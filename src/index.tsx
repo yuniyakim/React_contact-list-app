80c1d2cb5e4ccd7a50f532c8589e5d9cb9e6d866
Yuniya Kim <yuniyakim@gmail.com>
2022-08-17 19:35:54 +0300

    Provide store

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from "react-router-dom";
+import {Provider} from "react-redux";
+import {store} from "./store";
 import App from './App';
 import './index.css';
 
@@ -8,9 +10,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>
+  </Provider>
 );
