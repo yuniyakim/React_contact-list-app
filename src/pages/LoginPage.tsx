8121dd4f28e6a91b7856e3db074d6ac034908e13
Yuniya Kim <yuniyakim@gmail.com>
2022-08-18 22:45:04 +0300

    Add redirect after login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Login from "../components/Login";
 import {useAppActions, useAppSelector} from "../hooks";
 import {User} from "../types/auth";
+import {useNavigate} from "react-router-dom";
 
 const LoginPage = () => {
-  const {loading, error} = useAppSelector(state => state.auth);
+  const {userId, token, loading, error} = useAppSelector(state => state.auth);
   const {login} = useAppActions();
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (!loading) {
+      if (userId !== null && token !== null) {
+        return navigate('/contacts');
+      }
+    }
+  }, [loading])
 
   const handleSubmit = (formValues: User) => {
     login(formValues);
