efd32042aaf5ed71422e5e221daa4e1a182fc3b1
Yuniya Kim <yuniyakim@gmail.com>
2022-08-19 12:43:26 +0300

    Update button style

--- src/UI/Button.style.ts
+++ src/UI/Button.style.ts
@@ -4,16 +4,21 @@ import * as palette from "../theme/palette";
 
 export const StyledButton = styled(Button)`
   font-family: 'Open Sans', sans-serif;
-  height: 3em;
-  padding-left: 1em;
-  padding-right: 1em;
-  border: solid 1px ${palette.colors.border};
-  border-radius: 20px;
-  background-color: ${palette.colors.primary};
-  color: ${palette.colors.text};
-  cursor: pointer;
-  
+  border: none;
+  box-shadow: none !important;
+  background-color: ${props => props.danger ? palette.colors.red : props.warning ? palette.colors.secondary : palette.colors.primary};
+
   :hover {
-    background-color: ${palette.colors.primaryActive};
+    background-color: 
+            ${props => props.danger ? palette.colors.redHover : props.warning ? palette.colors.secondaryHover : palette.colors.primaryHover};
+  }
+
+  :focus, :active {
+    background-color: ${props => props.danger ? palette.colors.red : props.warning ? palette.colors.secondary : palette.colors.primary};
+
+    :hover {
+      background-color:
+              ${props => props.danger ? palette.colors.redHover : props.warning ? palette.colors.secondaryHover : palette.colors.primaryHover};
+    }
   }
 `
