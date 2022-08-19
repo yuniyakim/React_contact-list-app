0b81c813070908196da0ae04d3804e2b6cf7b2ea
Yuniya Kim <yuniyakim@gmail.com>
2022-08-19 12:42:58 +0300

    Update color names

--- src/theme/palette.ts
+++ src/theme/palette.ts
@@ -1,10 +1,10 @@
 export const colors = {
   primary: '#0070D4',
-  primaryActive: '#0060B6',
+  primaryHover: '#0060B6',
   secondary: '#FFB900',
-  secondaryActive: '#DCA000',
+  secondaryHover: '#DCA000',
   red: '#DD0000',
-  redActive: '#A00000',
+  redHover: '#A00000',
   text: '#FFFFFF',
   border: '#DDDDDD',
   background: '#F5F5F5',
