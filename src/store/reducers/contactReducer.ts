1401e109ec761cefcc7a51d7fc184e9ba9d6e1f5
Yuniya Kim <yuniyakim@gmail.com>
2022-08-22 00:32:49 +0300

    Remove multiple word search

--- src/store/reducers/contactReducer.ts
+++ src/store/reducers/contactReducer.ts
@@ -10,59 +10,56 @@ const initialState: ContactState = {
 export const contactReducer = (state = initialState, action: ContactAction): ContactState => {
   switch (action.type) {
     case ContactActionTypes.FETCH_CONTACTS: {
-      return {...state, loading: true, error: null};
+      return {...state, loading: true, error: null}
     }
     case ContactActionTypes.FETCH_CONTACTS_SUCCESS: {
-      return {...state, loading: false, contacts: action.payload, filteredContacts: action.payload};
+      return {...state, loading: false, contacts: action.payload, filteredContacts: action.payload}
     }
     case ContactActionTypes.FETCH_CONTACTS_ERROR: {
-      return {...state, loading: false, error: action.payload};
+      return {...state, loading: false, error: action.payload}
     }
     case ContactActionTypes.ADD_CONTACT: {
-      return {...state, loading: true, error: null};
+      return {...state, loading: true, error: null}
     }
     case ContactActionTypes.ADD_CONTACT_SUCCESS: {
-      return {...state, loading: false, contacts: [...state.contacts, action.payload]};
+      return {...state, loading: false, contacts: [...state.contacts, action.payload]}
     }
     case ContactActionTypes.ADD_CONTACT_ERROR: {
-      return {...state, loading: false, error: action.payload};
+      return {...state, loading: false, error: action.payload}
     }
     case ContactActionTypes.EDIT_CONTACT: {
-      return {...state, loading: true, error: null};
+      return {...state, loading: true, error: null}
     }
     case ContactActionTypes.EDIT_CONTACT_SUCCESS: {
-      return {...state, loading: false, contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact)};
+      return {...state, loading: false, contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact)}
     }
     case ContactActionTypes.EDIT_CONTACT_ERROR: {
-      return {...state, loading: false, error: action.payload};
+      return {...state, loading: false, error: action.payload}
     }
     case ContactActionTypes.DELETE_CONTACT: {
-      return {...state, loading: true, error: null};
+      return {...state, loading: true, error: null}
     }
     case ContactActionTypes.DELETE_CONTACT_SUCCESS: {
-      return {...state, loading: false, contacts: state.contacts.filter(contact => contact.id !== action.payload)};
+      return {...state, loading: false, contacts: state.contacts.filter(contact => contact.id !== action.payload)}
     }
     case ContactActionTypes.DELETE_CONTACT_ERROR: {
-      return {...state, loading: false, error: action.payload};
+      return {...state, loading: false, error: action.payload}
     }
     case ContactActionTypes.SEARCH_CONTACT: {
-      return {...state, loading: true, error: null};
+      return {...state, loading: true, error: null}
     }
     case ContactActionTypes.SEARCH_CONTACT_SUCCESS: {
-      const words = action.payload.trim().toLowerCase().split(' ');
       if (action.payload === '') {
-        return {...state, loading: false, filteredContacts: state.contacts};
+        return {...state, loading: false, filteredContacts: state.contacts}
       }
       return {
         ...state, loading: false, filteredContacts: state.contacts.filter(contact =>
-          words.every(word => {
-            return contact.surname.toLowerCase().includes(word) || contact.name.toLowerCase().includes(word);
-          })
-        )
+          contact.surname.toLowerCase().includes(action.payload.trim().toLowerCase()) ||
+          contact.name.toLowerCase().includes(action.payload.trim().toLowerCase()))
       }
     }
     case ContactActionTypes.SEARCH_CONTACT_ERROR: {
-      return {...state, loading: false, error: action.payload};
+      return {...state, loading: false, error: action.payload}
     }
     default: {
       return state;
